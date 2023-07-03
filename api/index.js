b43336f75524256a80ab0ebcdd8f965503d20762
Manas-0908 <manas.jha1991@gmail.com>
2023-07-03 19:35:16 +0530

    new commit

--- api/index.js
+++ api/index.js
@@ -38,11 +38,11 @@ app.use("/api/users",userRoute);
 app.use("/api/posts",postRoute);
 app.use("/api/categories",categoryRoute);
 
-/*app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});*/
+});
 
 app.listen(process.env.PORT || 5000, ()=>{
    console.log("Backend started at port 8800");
