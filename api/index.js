50ef5663527733a1aef92bb92b53979e882ac46e
Manas-0908 <manas.jha1991@gmail.com>
2023-07-03 19:53:39 +0530

    new

--- api/index.js
+++ api/index.js
@@ -38,9 +38,9 @@ app.use("/api/users",userRoute);
 app.use("/api/posts",postRoute);
 app.use("/api/categories",categoryRoute);
 
-app.use(express.static(path.join(__dirname, "/client")));
+/*app.use(express.static(path.join(__dirname, "/client")));
 
-/*app.get('*', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });*/
 
